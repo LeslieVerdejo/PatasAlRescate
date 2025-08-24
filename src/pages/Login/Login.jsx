97d90586e51777365b6f38d2ddd2ae8dbee24f8b
Deneken <deneken3109@gmail.com>
2025-08-24 16:34:53 -0400

    Se agrega validaciones al login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,37 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Login() {
+    
+    const [usuario, setUsuarios] = useState("")
+    const [password, setPassword] = useState("")
+
+function validarDatos(e) {
+    e.preventDefault()
+    if(usuario === "") {
+        alert("El usuario esta vacio")
+    } else if(password === "") {
+        alert("La contraseña esta vacia")
+    } else {
+        alert("Bienvenido")
+    }
+}
+
   return (
     <form>
         <div className="">
             <h1 className="pb-3">Bienvenido</h1>
         </div>
-      <div class="input-group mb-3">
-        <span class="input-group-text" id="basic-addon1">
+      <div className="input-group mb-3">
+        <span className="input-group-text" id="basic-addon1">
            🐶
         </span>
         <input
           type="text"
-          class="form-control"
+          className="form-control"
           placeholder="Usuario"
           aria-label="Username"
           aria-describedby="basic-addon1"
+          value={usuario}
+          onChange={(e) => setUsuarios(e.target.value)}
         />
       </div>
-      <div class="input-group flex-nowrap">
-        <span class="input-group-text" id="addon-wrapping">
+      <div className="input-group flex-nowrap">
+        <span className="input-group-text" id="addon-wrapping">
           🔐
         </span>
         <input
           type="Password"
-          class="form-control"
+          className="form-control"
           placeholder="Contraseña"
           aria-label="Username"
           aria-describedby="addon-wrapping"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
       </div>
       <div className="mt-3">
-        <button className="btn btn-primary">Ingresar</button>
+        <button onClick={validarDatos}  className="btn btn-primary">Ingresar</button>
       </div>
     </form>
   );
