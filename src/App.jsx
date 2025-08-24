c81557c8f3b3f03a3eae5a76933aeaf93503c20a
Leslie <leslie.verdejo11@gmail.com>
2025-08-24 16:04:19 -0400

    Nuevas modificaciones

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from './assets/react.svg'
 
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
-      
+      <Navbar/>
     </>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import "./Navbar.css"
 
 function Navbar() {
     return (
         <div>
             <>
+                <header>
+                    <div className="left">
+                        
+                    </div>
+                    <div className="right">
 
+                    </div>
+                </header>
             </>
         </div>
     )
