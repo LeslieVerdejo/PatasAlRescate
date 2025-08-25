1c66bd0f1259d26ed268edaa22d6e776e7cc9d85
Leslie <leslie.verdejo11@gmail.com>
2025-08-24 21:40:48 -0400

    Menu modificado

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Login from "./pages/Login/Login";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-      <Login />
+      {/* <Login /> */}
+      <Navbar/>
     </>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,12 +7,65 @@ function Navbar() {
             <>
                 <header>
                     <div className="left">
-                        
+                        <div className='menu-container'>
+                            <div className='menu'>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                            </div>
+                        </div>
+                        <div className='brand'>
+                            <img src="/src/assets/perro.jpg" alt="perro" className='logo'/>
+                            <span className='name'>apex</span>
+                        </div>
                     </div>
                     <div className="right">
-
+                        <a href="#">
+                            <img src="/src/assets/perro.jpg" alt="perro2" />
+                        </a>
+                        <a href="#">
+                            <img src="/src/assets/perro.jpg" alt="perro3" />
+                        </a>
+                        <a href="#">
+                            <img src="/src/assets/perro.jpg" alt="perro4" />
+                        </a>
+                        <img src="/src/assets/perro.jpg" alt="aca-va-el-user" className='user'/>
                     </div>
                 </header>
+                <div className='sidebar'>
+                    <nav>
+                            <li>
+                                <a href="#" className='search'>
+                                    <img src="/src/assets/perro.jpg" alt="" />
+                                    <span>Buscar</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className='selected'>
+                                    <img src="/src/assets/perro.jpg" alt="" />
+                                    <span>Productos</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="/src/assets/perro.jpg" alt="" />
+                                    <span>Ventas</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="/src/assets/perro.jpg" alt="" />
+                                    <span>Productos</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="/src/assets/perro.jpg" alt="" />
+                                    <span>Otro</span>
+                                </a>
+                            </li>
+                    </nav>
+                </div>
             </>
         </div>
     )
