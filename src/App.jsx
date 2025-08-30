790f3217fc4b56375b01c9495234777c0add18fe
Leslie <leslie.verdejo11@gmail.com>
2025-08-30 17:29:43 -0400

    Se crea Register

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Login from "./pages/Login/Login";
 import Navbar from "./components/Navbar/Navbar";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
     <>
       {/* <Login /> */}
-      <Navbar/>
+      {/* <Navbar/> */}
+      <Register/>
     </>
   );
 }
