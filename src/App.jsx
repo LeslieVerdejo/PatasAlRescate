fe7bbda7249f103d33914da8b77993073d474a0b
Deneken <deneken3109@gmail.com>
2025-08-31 19:16:04 -0400

    Se agregan rutas

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,18 @@ import Login from "./pages/Login/Login";
 import Navbar from "./components/Navbar/Navbar";
 import Register from "./pages/Register/Register";
 import Empleados from "./pages/Empleados/Empleados";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Home/HomePage";
 
 function App() {
   return (
     <>
-      {/* <Login /> */}
-      {/* <Navbar /> */}
-      {/* <Register /> */}
-      <Empleados />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/empleados" element={<Empleados />} />
+      </Routes>
     </>
   );
 }
---
+++ src/pages/Home/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <>
+      <div>Pagina Principal</div>
+      <div>Pagina Principal</div>
+      <div>Pagina Principal</div>
+    </>
+  );
+}
+
+export default HomePage;
